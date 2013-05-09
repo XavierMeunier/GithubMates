f39a69202f5709a281f01b0c263be572607076f2
XavierMeunier <meunier.xavier22@gmail.com>
2013-05-09 14:28:05 +0200

    Init with repos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubMates::Application.config.secret_key_base = '867fc5fc7c3857daf960b53750722f49a207579b8812fd1c150f33490e46e89bf2c358b34497a6d6f1855307a9423243bc182d3b82a7cb98bc972af04e284e9b'
